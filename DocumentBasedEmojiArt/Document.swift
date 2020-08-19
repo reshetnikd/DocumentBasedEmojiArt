32f1348a42a8982377ea65ed40037b574ad2f927
reshetnikd <dmitry.reshetnik@icloud.com>
2020-08-19 13:57:03 +0300

    Initial Commit

---
+++ DocumentBasedEmojiArt/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  DocumentBasedEmojiArt
+//
+//  Created by Dmitry Reshetnik on 19.08.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ DocumentBasedEmojiArt/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  DocumentBasedEmojiArt
+//
+//  Created by Dmitry Reshetnik on 19.08.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
